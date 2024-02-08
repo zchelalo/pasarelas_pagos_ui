f01d44b50104a7ca8246517aacd95b2321a38378
zchelalo <eduardosaavedra687@gmail.com>
2024-02-07 23:17:52 -0700

    logout with 401

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import { back_url } from '@/config/const'
 import { useToast } from '@/components/ui/use-toast'
+import { useAuth } from '@/contexts/AuthContext/useAuth'
 
 function useFetch({ url, method = 'GET', body = null, headers = {}}) {
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const auth = useAuth()
 
   const { toast } = useToast()
 
@@ -26,6 +28,10 @@ function useFetch({ url, method = 'GET', body = null, headers = {}}) {
             description: response.statusText,
             status: 'error'
           })
+
+          if (response.status === 401) {
+            auth.logout()
+          }
 
           throw { error: response.statusText }
         }
