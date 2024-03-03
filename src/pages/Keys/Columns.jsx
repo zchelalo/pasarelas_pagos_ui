b52878db12dd1f34a01f55e5019bd77b0843d2fb
zchelalo <eduardosaavedra687@gmail.com>
2024-03-03 16:09:49 -0700

    add columns and datatable now it's useful

---
+++ src/pages/Keys/Columns.jsx
@@ -0,0 +1,205 @@
+import { MoreHorizontal, ArrowUpDown } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Checkbox } from '@/components/ui/checkbox'
+
+import { MdDelete } from 'react-icons/md'
+
+import moment from 'moment'
+
+function Columns({ t, setOpenEditModal, setOpenAlertModal, setInfoKey}) {
+  const abrirModalEditar = (key) => {
+    setOpenEditModal(true)
+    setInfoKey(key)
+  }
+
+  const abrirAlertBorrar = (key) => {
+    setOpenAlertModal(true)
+    setInfoKey(key)
+  }
+
+  const columns = [
+    {
+      id: 'select',
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && 'indeterminate')
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label='Select all'
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label='Select row'
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      header: ({ column }) => {
+        return (
+          <Button
+            variant='ghost'
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            ID
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+      accessorKey: 'id'
+    },
+    {
+      header: ({ column }) => {
+        return (
+          <Button
+            variant='ghost'
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            {t('payment_gateway')}
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+      accessorKey: 'pasarela.nombre'
+    },
+    {
+      header: ({ column }) => {
+        return (
+          <Button
+            variant='ghost'
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            {t('clave')}
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+      accessorKey: 'clave'
+    },
+    {
+      header: ({ column }) => {
+        return (
+          <Button
+            variant='ghost'
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            {t('user')}
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+      accessorKey: 'usuario.nombre'
+    },
+    {
+      header: ({ column }) => {
+        return (
+          <Button
+            variant='ghost'
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            {t('created_at')}
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+      accessorKey: 'createdAt',
+      cell: info => info.getValue() ? moment(info.getValue()).utc(true).local().format('DD/MM/YYYY HH:mm:ss') : ''
+    },
+    {
+      header: ({ column }) => {
+        return (
+          <Button
+            variant='ghost'
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            {t('updated_at')}
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+      accessorKey: 'updatedAt',
+      cell: info => info.getValue() ? moment(info.getValue()).utc(true).local().format('DD/MM/YYYY HH:mm:ss') : ''
+    },
+    {
+      id: 'actions',
+      header: ({ table }) => {
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">{t('open_menu')}</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>{t('actions')}</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => console.log(table.getSelectedRowModel())}
+                className='cursor-pointer'
+              >
+                <MdDelete /> <span className='ml-2'>{t('delete_selected_columns')}</span>
+              </DropdownMenuItem>
+              {/* <DropdownMenuSeparator />
+              <DropdownMenuItem>View customer</DropdownMenuItem>
+              <DropdownMenuItem>View payment details</DropdownMenuItem> */}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )
+      },
+      cell: ({ row, table }) => {
+        const key = row.original
+        const paginaActual = table.options.state.pagination.pageIndex
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">{t('open_menu')}</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>{t('actions')}</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => navigator.clipboard.writeText(key.id)}
+                className='cursor-pointer'
+              >
+                {t('copy_id')}
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className='cursor-pointer'
+                onClick={() => abrirModalEditar({ ...key, setPageIndex: () => table.setPageIndex(paginaActual) })}
+              >
+                Generete a new secret key
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className='cursor-pointer'
+                onClick={() => abrirAlertBorrar({ ...key, setPageIndex: () => table.setPageIndex(paginaActual) })}
+              >
+                {t('delete')}
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )
+      }
+    }
+  ]
+
+  return columns
+}
+
+export { Columns }
--- src/pages/Keys/index.jsx
+++ src/pages/Keys/index.jsx
@@ -1,6 +1,49 @@
+import { useState } from 'react'
+
+import { DataTable } from '@/components/DataTable'
+import { Columns } from '@/pages/Keys/Columns'
+
+import { useAuth } from '@/contexts/AuthContext/useAuth'
+import { useFetch } from '@/hooks/useFetch'
+import { useTranslation } from 'react-i18next'
+
 function Keys() {
+  const { t } = useTranslation()
+  const { usuario, logout } = useAuth()
+  const { data: keys, setData: setKeys, loading, error } = useFetch({
+    url: usuario.rol === 'admin' ? '/v1/keys' : `/v1/keys?usuarioId=${usuario.id}`,
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${usuario.token}`
+    }
+  })
+
+  const [openCreateModal, setOpenCreateModal] = useState(false)
+  const [openEditModal, setOpenEditModal] = useState(false)
+  const [openAlertModal, setOpenAlertModal] = useState(false)
+  const [infoKey, setInfoKey] = useState(null)
+
+  console.log(keys)
+
   return (
-    <div>Keys</div>
+    <>
+      <div className='flex flex-wrap'>
+        <h1 className='text-4xl'>{t('keys')}</h1>
+      </div>
+      {loading && 'Cargando...'}
+      {!loading && ((!error && keys.length > 0) ? (
+        <DataTable
+          data={keys}
+          columns={Columns({
+            t,
+            setOpenEditModal,
+            setOpenAlertModal,
+            setInfoKey
+          })}
+        />
+      ) : 'No hay usuarios registrados')}
+    </>
   )
 }
 
